0f1f8059c8e8d02660df97b509a9ddf2f315170d
JimHofm52 <JimHofmann52@gmail.com>
2021-11-20 15:26:30 -0500

    Added Day 2

--- AOC2020/javaSrc/AoC2020.java
+++ AOC2020/javaSrc/AoC2020.java
@@ -7,6 +7,7 @@ Date Created: 12/5/2020
 import java.io.*;
 // import days.AllDays;
 import days.Day1;
+import days.Day2;
 import days.Day5;
 import days.Day6;
 import days.Day7;
@@ -24,7 +25,8 @@ import days.Day17;
 public class AoC2020 {
     public static void main(String[] arg) throws IOException{
 
-        Day1.update();  //<===== Change to match day
+        // Day1.update();  //<===== Change to match day
+        Day2.update();  //<===== Change to match day
         // Day5.update();  //<===== Change to match day
         // Day6.update();  //<===== Change to match day
         // Day7.update();  //<===== Change to match day
