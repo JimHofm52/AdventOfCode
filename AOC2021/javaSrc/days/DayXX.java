4a5488638a3216ac56116adc6e21ac2b0bf04412
JimHofm52 <JimHofmann52@gmail.com>
2021-12-06 15:33:09 -0500

    Day04 finished, Santa's going to be late.

--- AOC2021/javaSrc/days/DayXX.java
+++ AOC2021/javaSrc/days/DayXX.java
@@ -18,6 +18,8 @@ public class DayXX {
         fileInfo = ReadInput.getInputStr(fNum);   //Get input in an array for 1
         len = fileInfo.length;          //Length of input array
 
+        question1();    //Confirmed: 03-2635 * 1460 = 3847100   031-22 *  9 = 198
+        question2();    //Confirmed: 03-2735 * 1501 = 4105235   031-23 * 10 = 230
     }
 
     /**
