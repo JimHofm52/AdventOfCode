6ea186d962ad3d2d6b1768aa1de5ed7902beac6a
JimHofm52 <JimHofmann52@gmail.com>
2021-12-07 12:20:49 -0500

    Day05 complete.

--- AOC2021/javaSrc/AoC2021.java
+++ AOC2021/javaSrc/AoC2021.java
@@ -13,7 +13,9 @@ public class AoC2021 {
         // Day01.update();  //<===== Change to match day
         // Day02.update();  //<===== Change to match day
         // Day03.update();  //<===== Change to match day
-        Day04.update();  //<===== Change to match day
-        // Day05.update();  //<===== Change to match day
+        // Day04.update();  //<===== Change to match day
+        Day05.update();  //<===== Change to match day
+        // Day06.update();  //<===== Change to match day
+        // Day07.update();  //<===== Change to match day
     }
 }
