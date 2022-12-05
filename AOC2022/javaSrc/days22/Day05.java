436772af69e622a952932231d3376ea2c6e694c1
JimHofm52 <JimHofmann52@gmail.com>
2022-12-04 22:05:52 -0500

    Added Day05 2022

--- AOC2022/javaSrc/days22/Day05.java
+++ AOC2022/javaSrc/days22/Day05.java
@@ -4,28 +4,28 @@ import java.io.IOException;
 
 import util.ReadInput;
 
-public class DayXX {
+public class Day05 {
     private static String fileInfo[];
     private static int len;
 
     /** Constructor, not needed but used for standards. */
-    private DayXX(){}
+    private Day05(){}
 
     public static void update() throws IOException {
-        // String fNum = "01"; //Part1- ???   Part2- ???
-        String fNum = "011";//Part1- ???   Part2- ???
+        // String fNum = "05";  //Part1- ???   Part2- ???
+        String fNum = "051";    //Part1- ???   Part2- ???
         fileInfo = ReadInput.getInputStr(fNum);   //Get input in an array for 1
         len = fileInfo.length;          //Length of input array
 
-        question1();    //Confirmed: 03- ???   031- ???
-        question2();    //Confirmed: 03- ???   031- ???
+        question1();    //Confirmed: 03-???   031-???
+        question2();    //Confirmed: 03-???   031-???
     }
 
     /**
      * Question 1: Number of possible PWs meeting the criteria:
      */
     private static void question1() {
-        //Track ,  Confirmed: 04- ???   041- ???
+        //Track ,  Confirmed: 05- ???   051- ???
         // System.out.println("\nPart 1: ???: " + pwOKCnt);
     }
     
@@ -33,7 +33,7 @@ public class DayXX {
      * Question 2: Number of possible PWs meeting the criteria:
      */
     private static void question2() {
-        //Track ,  Confirmed: 04- ???   041- ???
+        //Track ,  Confirmed: 05- ???   051- ???
         // System.out.println("\nPart 2: ???: " + pwOKCnt);
     }
 }
