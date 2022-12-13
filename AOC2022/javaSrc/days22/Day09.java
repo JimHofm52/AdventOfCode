4083aaa9d04d455103ab9d3f11d6dbc83b87ccf0
JimHofm52 <JimHofmann52@gmail.com>
2022-12-13 10:53:25 -0500

    Setup Day09.

---
+++ AOC2022/javaSrc/days22/Day09.java
@@ -0,0 +1,39 @@
+package days22;
+
+import java.io.IOException;
+
+import util.ReadInput;
+
+public class Day09 {
+    private static String fileInfo[];
+    private static int len;
+
+    /** Constructor, not needed but used for standards. */
+    private Day09(){}
+
+    public static void update() throws IOException {
+        // String fNum = "09"; //Part1- ???   Part2- ???
+        String fNum = "091";//Part1- ???   Part2- ???
+        fileInfo = ReadInput.getInputStr(fNum);   //Get input in an array for 1
+        len = fileInfo.length;          //Length of input array
+
+        question1();    //Confirmed: 09- ???   091- ???
+        question2();    //Confirmed: 09- ???   091- ???
+    }
+
+    /**
+     * Question 1: Number of possible PWs meeting the criteria:
+     */
+    private static void question1() {
+        //Track ,  Confirmed: 09- ???   091- ???
+        // System.out.println("\nPart 1: ???: " + pwOKCnt);
+    }
+    
+    /**
+     * Question 2: Number of possible PWs meeting the criteria:
+     */
+    private static void question2() {
+        //Track ,  Confirmed: 09- ???   091- ???
+        // System.out.println("\nPart 2: ???: " + pwOKCnt);
+    }
+}
