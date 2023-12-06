957fd5863ea6a364771c4a7d881cd372997a2f38
JimHofm52 <JimHofmann52@gmail.com>
2023-12-06 11:22:04 -0500

    23 Setup for Day 5

---
+++ AoC2023/javasrc/days23/Day05.java
@@ -0,0 +1,39 @@
+package days23;
+
+import java.io.IOException;
+
+import util.ReadInput;
+
+public class Day05 {
+    private static String fileInfo[];
+    private static int len;
+
+    /** Constructor, not needed but used for standards. */
+    private Day05(){}
+
+    public static void update() throws IOException {
+        // String fNum = "05"; //Part1- ???   Part2- ???
+        String fNum = "051";//Part1- ???   Part2- ???
+        fileInfo = ReadInput.getInputStr(fNum);   //Get input in an array for 1
+        len = fileInfo.length;          //Length of input array
+
+        question1();    //Confirmed: 05- ???   051- ???
+        question2();    //Confirmed: 05- ???   051- ???
+    }
+
+    /**
+     * Question 1: Number of possible PWs meeting the criteria:
+     */
+    private static void question1() {
+        //Track ,  Confirmed: 05- ???   051- ???
+        // System.out.println("\nPart 1: ???: " + pwOKCnt);
+    }
+    
+    /**
+     * Question 2: Number of possible PWs meeting the criteria:
+     */
+    private static void question2() {
+        //Track ,  Confirmed: 05- ???   051- ???
+        // System.out.println("\nPart 2: ???: " + pwOKCnt);
+    }
+}
