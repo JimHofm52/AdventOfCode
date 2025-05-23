a64322e07cb9dff4f964d521276eaaaeb698ac40
JimHofm52 <JimHofmann52@gmail.com>
2025-05-23 18:27:08 -0400

    Setup for Day08

--- aoc2024/javasrc/AoC2024.java
+++ aoc2024/javasrc/AoC2024.java
@@ -19,9 +19,9 @@ public class AoC2024 {
         // Day04.update();  //<===== Change to match day   //0.226 s.
         // Day05.update();  //<===== Change to match day   //221 s.
         // Day06.update();  //<===== Change to match day   //19.533 s.
-        // Day06A.update();  //<===== Change to match day   //18.375 s.
-        Day07.update();  //<===== Change to match day   //1.646 s.
-        // Day08.update();  //<===== Change to match day   //7.549 s.
+        // Day06A.update();  //<===== Change to match day  //18.375 s.
+        // Day07.update();  //<===== Change to match day   //1.646 s.
+        Day08.update();  //<===== Change to match day   //7.549 s.
         // Day09.update();  //<===== Change to match day   //0.157 s.
         // Day10.update();  //<===== Change to match day
         // Day11.update();  //<===== Change to match day
--- aoc2024/javasrc/days24/Day08.java
+++ aoc2024/javasrc/days24/Day08.java
@@ -9,25 +9,26 @@ public class Day08 {
     private static int len;
 
     /** Constructor, not needed but used for standards. 
-     * <p>Started Dec 8, 2024,  Finished on Dec 8, 2024.
+     * <p>Started May 25, 2025,  Finished on May ?? 2025.
      * <p>Took ~xx hour.  Runtime 0.yyy S.
     */
     private Day08(){}
 
     public static void update() throws IOException {
-        String fNum = "08";//Part1- ???   Part2- ???
+        String fNum = "081";//Part1- ???   Part2- ???
         fileInfo = ReadWriteFiles.getInputStr(fNum);   //Get input in an array for 1
         len = fileInfo.length;          //Length of input array
 
-        question1();    //Confirmed: 08- ???   081- ???
+        question1();    //Confirmed: 08- ???   081- 14
         question2();    //Confirmed: 08- ???   081- ???
     }
 
     /**
-     * Question 1: Number of possible PWs meeting the criteria:
+     * Question 1: Calculate the impact of the signal. 
+     * How many unique locations within the bounds of the map contain an antinode?
      */
     private static void question1() {
-        //Track ,  Confirmed: 08- ???   081- ???
+        //Track ,  Confirmed: 08- ???   081- 14
         // System.out.println("\nPart 1: ???: " + pwOKCnt);
     }
     
