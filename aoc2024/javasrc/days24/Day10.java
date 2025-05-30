80fa324c1df8b815969b2c84aa0ca854fbdb910c
JimHofm52 <JimHofmann52@gmail.com>
2025-05-29 21:55:17 -0400

    2024 Day10 part 2 DONE!

--- aoc2024/javasrc/days24/Day10.java
+++ aoc2024/javasrc/days24/Day10.java
@@ -9,23 +9,22 @@ import util.ReadWriteFiles;
 public class Day10 {
     private static String fileInfo[];
     private static int len;
-    private static enum Dir {R, D, L, U; }
 
     /** Constructor, not needed but used for standards. 
      * <p>Started May 28, 2025,  Finished on May 29, 2025.
-     * <p>Took ~xx hour.  Runtime 0.yyy S.
+     * <p>Took ~12 hour.  Runtime 0.yyy S.
     */
     private Day10(){}
 
     public static void update() throws IOException {
-        String fNum = "10";//Part1- 667   Part2- ???
+        String fNum = "10";//Part1- 667   Part2- 1344
         fileInfo = ReadWriteFiles.getInputStr(fNum);   //Get input in an array for 1
         len = fileInfo.length;          //Length of input array
 
         Trailhead[] myTHs = findTrailHead(fileInfo);
 
         question1(myTHs, fileInfo);    //Confirmed: 10- 667   101- 36
-        question2();    //Confirmed: 10- ???   101- ???
+        question2(myTHs);    //Confirmed: 10- 1344   101- 81
     }
 
     /**
@@ -36,25 +35,32 @@ public class Day10 {
      */
     private static void question1(Trailhead[] thIn, String[] map) {
         int[] thLoc = new int[2];
-        int trailCnt = 0;
+        int trailTailsCnt = 0;
         for(Trailhead th : thIn){
             thLoc[0] = th.getRow();
             thLoc[1] = th.getCol();
             findTrails(map, th, thLoc, '0');
-            trailCnt += th.getTrailCnt();
+            trailTailsCnt += th.getTrailCnt();
         }
         //Track ,  Confirmed: 10- 667   101- 36 = 5, 6, 5, 3, 1, 3, 5, 3, and 5
-        System.out.println("\nPart 1: Total number of trails: " + trailCnt);
+        System.out.println("\nPart 1: Total number of trails: " + trailTailsCnt);
     }
     
     /**
-     * Question 2: Number of possible PWs meeting the criteria:
+     * Question 2: What is the sum of the ratings of all trailheads?
      */
-    private static void question2() {
-        //Track ,  Confirmed: 10- ???   101- ???
-        // System.out.println("\nPart 2: ???: " + pwOKCnt);
+    private static void question2(Trailhead[] thIn) {
+        int totTrails = 0;
+        for(Trailhead th : thIn) totTrails += th.getTotTrails();
+        //Track ,  Confirmed: 10- 1344   101- 81
+        System.out.println("\nPart 2: Total trails: " + totTrails);
     }
 
+    /**
+     * Fing all the Trailheads in the map.
+     * @param strIn
+     * @return arrary of trailheads
+     */
     private static Trailhead[] findTrailHead(String[] strIn){
         Trailhead[] thIn = new Trailhead[0];
         for(int r = 0; r < strIn.length; r++){
@@ -68,6 +74,13 @@ public class Day10 {
         return thIn;
     }
 
+    /**
+     * Find all trails.  Both the trailtails and total trails.
+     * @param mapIn
+     * @param th
+     * @param prvLoc
+     * @param prvHeight
+     */
     private static void findTrails(String[] mapIn, Trailhead th, int[] prvLoc, int prvHeight){
         int nxtHeight = prvHeight++;
         int[] nxtLoc;
@@ -79,6 +92,7 @@ public class Day10 {
                 if(nxtHeight == prvHeight){
                     if(nxtHeight == '9'){
                         th.addTrailtail(nxtLoc);
+                        th.setTotTeails(th.getTotTrails() + 1);
                     }else{
                         findTrails(mapIn, th, nxtLoc, nxtHeight);
                     }
@@ -87,6 +101,12 @@ public class Day10 {
         }
     }
 
+    /**
+     * Add direction (right, down, left & up) to location
+     * @param loc
+     * @param dir
+     * @return next location
+     */
     private static int[] addDir(int[] loc, int dir){
         switch(dir){
             case 0: return new int[] {loc[0], loc[1] + 1};
@@ -99,6 +119,12 @@ public class Day10 {
         }
     }
 
+    /**
+     * Check if location is on the map.
+     * @param loc
+     * @param map
+     * @return true if in map boundry, else false.
+     */
     private static boolean isOnMap(int[] loc, String[] map){
         return (loc[0] >= 0 && loc[0] < map[0].length() &&
                 loc[1] >= 0 && loc[1] < map[0].length());
--- aoc2024/javasrc/type/Trailhead.java
+++ aoc2024/javasrc/type/Trailhead.java
@@ -9,6 +9,7 @@ public class Trailhead {
     private int trail;
     private int trailCnt = 0;
     private int[] trailtails;
+    private int totTrails = 0;
 
     /**
      * Constructor for a trailhead.  Will be assigned an incremental BlockID 
@@ -21,15 +22,36 @@ public class Trailhead {
         th_rc = row * 100 + col;
     }
 
+    /** @return decmpressed row */
     public int getRow(){ return th_rc / 100; }
+    /** @return decmpressed row */
     public int getCol(){ return th_rc % 100; }
+    /**  @return compressed trailhead location, rc */
     public int getRC(){ return th_rc; }
+    /**  @return Trailtail count */
     public int getTrailCnt(){ return trailtails.length; }
+    /** @return Total trails */
+    public int getTotTrails(){ return totTrails; }
+    /**
+     * Set the total trail count to number.
+     * @param num
+     */
+    public void setTotTeails(int num){ totTrails = num; }
 
+    /**
+     * Add a trailtail if it doesn't a;ready exists.  Public facing.
+     * @param loc
+     */
     public void addTrailtail(int[] loc){
         trailtails = addTrailtail(loc[0] * 100 + loc[1], trailtails);
     }
 
+    /**
+     * Helper file to add trailtails
+     * @param rc
+     * @param tts
+     * @return
+     */
     private static int[] addTrailtail(int rc, int[] tts){
         boolean exists = false;
         if(tts == null){
@@ -45,5 +67,4 @@ public class Trailhead {
         }
         return tts;
     }
-
 }
